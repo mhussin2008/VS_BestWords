f28a1910af80e4a311cfa2eee35082cb3403770d
MohamedHussin <mhussin2008@yahoo.com>
2024-02-22 17:14:38 +0200

    Add project files.

---
+++ QuranBestWords/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.BtnIncrease = New System.Windows.Forms.Button()
+        Me.BtnDecrease = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(156, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(577, 586)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 31)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "get mp3 file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(12, 618)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(138, 46)
+        Me.AxWindowsMediaPlayer1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 107)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(138, 44)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "play now"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 340)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(138, 31)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Load Picture"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 504)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(138, 31)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "adjust screen"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'BtnIncrease
+        '
+        Me.BtnIncrease.Location = New System.Drawing.Point(12, 183)
+        Me.BtnIncrease.Name = "BtnIncrease"
+        Me.BtnIncrease.Size = New System.Drawing.Size(138, 44)
+        Me.BtnIncrease.TabIndex = 6
+        Me.BtnIncrease.Text = "<<<<<------"
+        Me.BtnIncrease.UseVisualStyleBackColor = True
+        '
+        'BtnDecrease
+        '
+        Me.BtnDecrease.Location = New System.Drawing.Point(12, 251)
+        Me.BtnDecrease.Name = "BtnDecrease"
+        Me.BtnDecrease.Size = New System.Drawing.Size(138, 44)
+        Me.BtnDecrease.TabIndex = 7
+        Me.BtnDecrease.Text = "------>>>>>"
+        Me.BtnDecrease.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(825, 830)
+        Me.Controls.Add(Me.BtnDecrease)
+        Me.Controls.Add(Me.BtnIncrease)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents BtnIncrease As Button
+    Friend WithEvents BtnDecrease As Button
+End Class
---
+++ QuranBestWords/Form1.vb
@@ -0,0 +1,95 @@
+﻿
+
+Public Class Form1
+    Public pageNumber As Integer
+    Public maxPage As Integer
+    Public minPage As Integer
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Dim x As AxWMPLib.AxWindowsMediaPlayer
+
+
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+
+
+
+        AxWindowsMediaPlayer1.URL = "D:\VB_Projects\QuranBestWords\QuranBestWords\mp3_original\1-split\1_silence_01.mp3"
+
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Image = Image.FromFile("D:\VB_Projects\QuranBestWords\QuranBestWords\images\32.png")
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        pageNumber = 32
+        maxPage = 41
+        minPage = 32
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim ScreenW, ScreenH, FormW, FormH As Double
+        ScreenW = SystemInformation.PrimaryMonitorSize.Width
+        ScreenH = SystemInformation.PrimaryMonitorSize.Height
+        Debug.WriteLine(ScreenW)
+        Debug.WriteLine(ScreenH)
+
+        Me.Height = SystemInformation.PrimaryMonitorSize.Height
+        Me.SetDesktopLocation((ScreenW - Me.Width) / 2, 0)
+        PictureBox1.Width = Me.Width - 200
+        PictureBox1.Height = Me.Height - 100
+
+
+
+
+
+
+    End Sub
+
+    Private Sub BtnIncrease_Click(sender As Object, e As EventArgs) Handles BtnIncrease.Click
+        If pageNumber >= maxPage Then
+            Exit Sub
+        End If
+
+        pageNumber = pageNumber + 1
+
+        getPage()
+
+    End Sub
+
+    Private Sub getPage()
+        PictureBox1.Image = Image.FromFile("D:\VB_Projects\QuranBestWords\QuranBestWords\images\" + pageNumber.ToString + ".png")
+        '  Throw New NotImplementedException()
+    End Sub
+
+    Private Sub BtnDecrease_Click(sender As Object, e As EventArgs) Handles BtnDecrease.Click
+        If pageNumber <= minPage Then
+            Exit Sub
+        End If
+        pageNumber = pageNumber - 1
+        getPage()
+    End Sub
+End Class
